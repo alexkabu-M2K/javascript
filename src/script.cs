4e854bad5a8586c8c4cfa8ccad126ae3b0258b5c
Alex Kabu <alexkabu@outlook.com>
2025-06-21 10:43:07 -0300

    comentary examples

--- src/script.cs
+++ src/script.cs
@@ -1,3 +1,16 @@
 
-console.log("Hello World");
+// Comentário de Linha 
+
+console.log("Hello World"); // Logando " Hello World" no console do Computador
+
+/* Comentário de Bloco Simples */
+
+/*
+console.log ()"Olá, Mundo");
+*/
+
+/**
+ * comentário de bloco com marcador
+ */
+
 
