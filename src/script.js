4d7b3a96e04148cec7ff2abccccbcc56112543ca
Alex Kabu <alexkabu@outlook.com>
2025-07-05 10:52:03 -0300

    working with javascript primitive types

---
+++ src/script.js
@@ -0,0 +1,63 @@
+
+// TIPOS DE DADOS
+
+// string
+var name = "Alex";
+var surname = "Bessa";
+
+// number
+var age = 29; // inteiro (whole)
+var weight = 85.6; // fracionado (float)
+
+// boolean (boleado)
+var active = true; // atividade > verdadeira
+var permission = false; // permissões > falsa
+
+// undefined (não definido)
+var contains;
+console.log(contains);
+
+// null (nulo)
+var data = null;
+
+// array (lista)
+var values = [1, "Alex", true, null];
+
+// object literal
+var person = {nome:"Alex", age: 29};
+
+var person = {
+    name: "Alex",
+    age: 29
+};
+
+//function
+var message = function() {};
+
+// arr (lista)
+var arr = [
+    [1,2,3]
+    [
+        ["Alex", "Marcos", 'Daniel'],
+        [29, 40, 35],
+    ],
+    [true, false],
+];
+
+console.log(arr[1[1]])
+
+var arr = [
+    {
+        name: "Alex",
+        age: 40
+    },
+    { 
+        name: "Marcos",
+        age: 35
+    }
+];
+
+console.log(arr[1].name);
+
+
+
